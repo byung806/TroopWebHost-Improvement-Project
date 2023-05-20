78aae5a0d68d8b0b975c0022425e80f360db604f
Bryan Yung <byung806@gmail.com>
2023-05-20 18:33:51 -0400

    Add code to log into TroopWebHost and establish a session

--- get_data.py
+++ get_data.py
@@ -1,1 +1,37 @@
 import requests
+
+
+def log_in(session: requests.Session):
+    with open('login.txt', 'r') as f:
+        username, password = f.read().split('\n')
+
+    payload = {
+        'menuopenflag': 'N',
+        'Selected_Action': 'login',
+        'Menu_Item_ID': 49792,
+        'Form_ID': 7323,
+        'Pass': 1,
+        'Stack': 1,
+        'ChildRowID': 0,
+        'Current_URL': 'https://www.troopwebhost.org/formCustom.aspx?Menu_Item_ID=49792&Custom_Form_ID=1',
+        'User_Login': username,
+        'User_Password': password,
+        'Language_ID': 1,
+        'Page_Layout': 1,
+        'Report_option': 2,
+        'Custom_Form_ID': 1,
+        'FirstControl': 'User_Login',
+    }
+
+    cookies = {
+        'Application_ID': '1338',
+    }
+
+    login_url = 'https://www.troopwebhost.org/formCustom.aspx'
+
+    return session.post(login_url, data=payload, cookies=cookies)
+
+
+
+with requests.Session() as session:
+    p = log_in(session)
