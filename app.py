fe6f640cba3a2a7aa6ecc9ffc1827eac2b263df8
Bryan Yung <byung806@gmail.com>
2023-05-23 19:42:37 -0400

    Implement basic login screen UI & screen switching

--- app.py
+++ app.py
@@ -1,36 +1,65 @@
 from tkinter import Label, Tk, Frame, Entry, Button
 
 class App(Tk):
+    LOGIN_SCREEN = 0
+    DATA_VISUALIZATION_SCREEN = 1
+
+
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         container = Frame(self)
-        container.pack(side = 'top', fill = 'both', expand = True)
-        container.title('TroopWebHost Improvement Project')
+        container.pack(side='top', fill='both', expand=True)
+        self.title('TroopWebHost Improvement Project')
 
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
 
         self.screens = {}
-        login_frame = LoginScreen(container)
+        self.screens[App.LOGIN_SCREEN] = LoginScreen(container, controller=self)
+        self.screens[App.DATA_VISUALIZATION_SCREEN] = DataVisualizationScreen(container, controller=self)
+
+        for screen_name in self.screens:
+            self.screens[screen_name].grid(row=0, column=0, sticky='nsew')
+
+        self.switch_screen_to(App.LOGIN_SCREEN)
+
     
-    def show_screen(self, name):
+    def switch_screen_to(self, name):
         screen = self.screens[name]
         screen.tkraise()
 
 
 class LoginScreen(Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
-        login_label = Label(self, text='Sign In', fg='#57a1f8', bg='white')
-        username_label = Label(self, text='Username', fg='#57a1f8', bg='white')
+        def authenticate():
+            username = username_entry.get()
+            password = password_entry.get()
+            controller.switch_screen_to(App.DATA_VISUALIZATION_SCREEN)
+
+        login_label = Label(self, text='Sign In')
+        username_label = Label(self, text='Username')
         username_entry = Entry(self)
-        password_label = Label(self, text='Password', fg='#57a1f8', bg='white')
+        password_label = Label(self, text='Password')
         password_entry = Entry(self, show='*')
-        login_button = Button(self, text='Login', fg='#57a1f8', bg='white', command=authenticate)
+        login_button = Button(self, text='Login', command=authenticate)
+
+        login_label.grid(row=0, column=0, columnspan=2, sticky='nsew', pady=40)
+        username_label.grid(row=1, column=0)
+        username_entry.grid(row=1, column=1, pady=20)
+        password_label.grid(row=2, column=0)
+        password_entry.grid(row=2, column=1, pady=20)
+        login_button.grid(row=3, column=0, columnspan=2, pady=30)
+        
+
+class DataVisualizationScreen(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        screen_label = Label(self, text='Data Visualization Screen')
+        screen_label.pack()
     
-    def authenticate():
-        pass
 
 app = App()
 app.mainloop()
