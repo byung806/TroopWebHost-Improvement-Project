dec9822c8756293e004a9ca6d9cdb13f9b881e2e
Bryan Yung <byung806@gmail.com>
2023-06-05 18:29:44 -0400

    Fix bug with incorrect header names in SortableTreeview

--- custom_elements.py
+++ custom_elements.py
@@ -52,23 +52,24 @@ class PlaceholderEntry(Entry):
 
 # Custom class to create a sortable chart
 class SortableTreeview(Treeview):
-    def __init__(self, columns=(), master=None, *args, **kwargs):
-        super().__init__(master, columns=columns, *args, **kwargs)
+    def __init__(self, master=None, columns={}, *args, **kwargs):
+        super().__init__(master, columns=columns.keys(), *args, **kwargs)
+        self.col_names = columns
         # Sort by increasing order on first click
         for col in columns:
-            self.heading(col, text=col, command=lambda col_copy=col: self.sort_by(col_copy, reverse=False))
+            self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(col_copy, reverse=False), anchor='center')
 
 
     # Sort chart by a specific column
     def sort_by(self, col, reverse=False):
         # Get and sort specific column, keeping track of which row each item was originally in
         column = [(iid, self.set(iid, col)) for iid in self.get_children()]
-        column.sort(reverse, key=lambda x: x[1])
+        column.sort(reverse=reverse, key=lambda x: x[1])
     
         # Rearrange original rows in the chart based on sort
         for i, (iid, value) in enumerate(column):
-            self.move(iid, parent=None, index=i)
+            self.move(iid, parent='', index=i)
 
         # Set sort command to sort other way on next click
-        self.heading(col, text=col, command=lambda col_copy=col: self.sort_by(col_copy, not reverse))
+        self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(col_copy, not reverse), anchor='center')
         
