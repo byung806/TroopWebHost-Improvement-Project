e9dc0651acf52f1fec8dad4519d23bbbe09bc95d
Bryan Yung <byung806@gmail.com>
2023-06-06 15:04:18 -0400

    Convert get_data to be asynchronous using asyncio & aiohttp

--- get_data.py
+++ get_data.py
@@ -1,10 +1,11 @@
-import requests
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 import numpy as np
 
 
 # Log in using a POST request in a session
-def log_in(session, username, password):
+async def log_in(session, username, password):
     payload = {
         'Selected_Action': 'login',
         'Menu_Item_ID': 49792,
@@ -17,36 +18,35 @@ def log_in(session, username, password):
 
     LOGIN_URL = 'https://www.troopwebhost.org/formCustom.aspx'
 
-    p = session.post(LOGIN_URL, data=payload)
-    # Returns whether or not login was successful
-    return 'Log Off' in p.text
+    async with session.post(LOGIN_URL, data=payload) as response:
+        assert response.status == 200
+        # Returns whether or not login was successful
+        return await 'Log Off' in response.text()
 
 
 # Use GET request to get HTML at an URL (in a logged in session)
-def get_html_page(session, url):
-    response = session.get(url)
-    return response.text
+async def get_html_page(session, url):
+    async with session.get(url) as response:
+        assert response.status == 200
+        return await response.text()
 
 
-def get_logged_in_session(username, password):
-    with requests.Session() as session:
-        # Application ID of Troop 1094 Darnestown
-        session.cookies.set('Application_ID', '1338')
-        login_successful = log_in(session, username, password)
+async def get_logged_in_session(username, password):
+    # Application ID of Troop 1094 Darnestown is 1338
+    cookies = {'RowsPerPage': 'ALL', 'Application_ID': '1338'}
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        login_successful = await log_in(session, username, password)
         # Returns logged in session or None if login failed
         return session if login_successful else None
         
 
 # Main function to scrape all the data and format it
-def get_data(logged_in_session):
+async def get_data(logged_in_session):
     ADULT_TRAINING_URL = 'https://www.troopwebhost.org/FormList.aspx?Menu_Item_ID=45888&Stack=0'
     SEND_EMAIL_URL = 'https://www.troopwebhost.org/FormDetail.aspx?Menu_Item_ID=45961&Stack=1'
-    
-    # Cookie to request all data on one page
-    logged_in_session.cookies.set('RowsPerPage', 'ALL')
 
-    adult_trainings_page = get_html_page(logged_in_session, ADULT_TRAINING_URL)
-    send_email_page = get_html_page(logged_in_session, SEND_EMAIL_URL)
+    adult_trainings_page = await get_html_page(logged_in_session, ADULT_TRAINING_URL)
+    send_email_page = await get_html_page(logged_in_session, SEND_EMAIL_URL)
 
     # Columns of the data with names, for use when parsing
     columns = {
@@ -90,4 +90,4 @@ def get_data(logged_in_session):
 
 # For testing
 if __name__ == '__main__':
-    get_data()
+    get_data(get_logged_in_session('sdraisen', 'My3scouts'))
