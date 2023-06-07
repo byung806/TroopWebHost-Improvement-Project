efcecc328fbc2f2b3af076bb246207015e576cff
Bryan Yung <byung806@gmail.com>
2023-06-07 18:11:57 -0400

    Implement actual sorting by date using datetime

--- custom_elements.py
+++ custom_elements.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import END, PhotoImage
 from tkinter.ttk import Entry, Treeview
+from datetime import datetime
 
 
 # Custom class to create entry boxes with a placeholder
@@ -16,15 +17,15 @@ class PlaceholderEntry(Entry):
         self.bind('<FocusOut>', self.fill_placeholder)
         self.fill_placeholder()
 
-
     # Resets entry box after a bad password (clears text and sets focus)
+
     def reset(self, *_):
         self.delete(0, END)
         self.has_content = True
         self.focus_set()
 
-
     # Clears placeholder text in entry, called when user clicks into entry box
+
     def clear_placeholder(self, *_):
         if not self.has_content:
             self.has_content = True
@@ -32,8 +33,8 @@ class PlaceholderEntry(Entry):
             self.config(show=self.show_original)
             self.delete(0, END)
 
-
     # Fills placeholder text in entry, called when user clicks out of entry & box is empty
+
     def fill_placeholder(self, *_):
         if super().get() == '':
             self.insert(0, self.placeholder)
@@ -41,8 +42,8 @@ class PlaceholderEntry(Entry):
             self.config(show='')
             self.has_content = False
 
-
     # Override original get function to return '' when there's a placeholder
+
     def get(self):
         content = super().get()
         if self.has_content:
@@ -58,22 +59,36 @@ class SortableTreeview(Treeview):
         self['show'] = show
         # Sort by increasing order on first click
         for col in columns:
-            self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(col_copy, reverse=False), anchor='center')
-
+            self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(
+                col_copy, reverse=False), anchor='center')
 
     # Sort chart by a specific column
+
     def sort_by(self, col, reverse=False):
         # Get and sort specific column, keeping track of which row each item was originally in
         column = [(iid, self.set(iid, col)) for iid in self.get_children()]
-        column.sort(reverse=reverse, key=lambda x: x[1])
-    
+        def normal_key(x): return x[1]
+        if reverse:
+            # Sort by date (latest to earliest), setting people without a date to a date far in the past so they show up at the bottom
+            def dates_key(d): return datetime.strptime(
+                '12/22/1900' if not d[1] else d[1], r'%m/%d/%Y')
+        else:
+            # Sort by date (earliest to latest), setting people without a date to a date far in the future
+            def dates_key(d): return datetime.strptime(
+                '12/22/2050' if not d[1] else d[1], r'%m/%d/%Y')
+        column_is_date = self.col_names[col] == 'Expiry Date'
+        # Sort by normal key if column is not a date, else sort by the date key
+        column.sort(reverse=reverse,
+                    key=dates_key if column_is_date else normal_key)
+
         # Rearrange original rows in the chart based on sort
         for i, (iid, value) in enumerate(column):
             self.move(iid, parent='', index=i)
 
         # Set sort command to sort other way on next click
-        self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(col_copy, not reverse), anchor='center')
-        
+        self.heading(col, text=self.col_names[col], command=lambda col_copy=col: self.sort_by(
+            col_copy, not reverse), anchor='center')
+
 
 # Custom class to create checkable rows in a Treeview
 class CheckableSortableTreeview(SortableTreeview):
