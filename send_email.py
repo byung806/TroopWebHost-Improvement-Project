b4cd4114c7afff98d3e721a394715d833aae3ea1
Bryan Yung <byung806@gmail.com>
2023-06-06 23:55:22 -0400

    Create email sending file

---
+++ send_email.py
@@ -0,0 +1,10 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+me = ''
+recipients = ['smcs2025.cbtsoftware@gmail.com']
+
+recipients = ', '.join(recipients)
+
