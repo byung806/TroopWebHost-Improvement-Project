8ead21ce719178892fc8633ef0a84ac0cc8814af
Bryan Yung <byung806@gmail.com>
2023-06-06 22:26:14 -0400

    Request all rows instead of 100

--- get_data.py
+++ get_data.py
@@ -42,6 +42,9 @@ def get_data(logged_in_session):
     ADULT_TRAINING_URL = 'https://www.troopwebhost.org/FormList.aspx?Menu_Item_ID=45888&Stack=0'
     SEND_EMAIL_URL = 'https://www.troopwebhost.org/FormDetail.aspx?Menu_Item_ID=45961&Stack=1'
 
+    # Cookie to request all data on one page
+    logged_in_session.cookies.set('RowsPerPage', 'ALL')
+
     adult_trainings_page = get_html_page(logged_in_session, ADULT_TRAINING_URL)
     send_email_page = get_html_page(logged_in_session, SEND_EMAIL_URL)
 
