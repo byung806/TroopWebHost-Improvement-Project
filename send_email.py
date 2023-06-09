0c145a7544e239a51790355296ae460745715418
Bryan Yung <byung806@gmail.com>
2023-06-09 09:37:27 -0400

    Separate different exceptions

--- send_email.py
+++ send_email.py
@@ -6,6 +6,8 @@ from email.mime.text import MIMEText
 
 # Send email 
 def send_email(email, password, bcc_recipients, subject, body) -> bool:
+    if not email or not password or not bcc_recipients:
+        return False, 'Email failed to send. Please fill in the username, app password, and add at least 1 recipient.'
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = email
@@ -20,11 +22,11 @@ def send_email(email, password, bcc_recipients, subject, body) -> bool:
             # Login using email and app password
             smtp_server.login(email, password)
             smtp_server.sendmail(email, bcc_recipients, msg.as_string())
-        return True
+        return True, 'Email sent!'
+    except TimeoutError:
+        # Socket connection failed
+        return False, 'Email failed to send. Please make sure you\'re connected to the internet.'
+    except SMTPAuthenticationError:
+        return False, 'Email failed to send. Incorrect username or password.'
     except:
-        # Failed to send
-        return False
-
-
-if __name__ == '__main__':
-    send_email('byung806@gmail.com', input(), ['smcs2025.cbtsoftware@gmail.com', 'bryandragonyayyayyay@gmail.com'], 'Test', 'Hello this worked')
+        return False, 'Email failed to send.'
